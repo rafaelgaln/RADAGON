c875c1c7d6e7970756fc1cdf6d312263146940ff
rafaelgaln <rafael.galn@educa.madrid.org>
2024-05-13 15:01:00 +0200

    Día 5: Pregunta lengua.

--- LaPractica3T/src/juego/main/MainTest.java
+++ LaPractica3T/src/juego/main/MainTest.java
@@ -6,12 +6,9 @@ import juego.utilidades.GestionPreguntas;
 
 import java.io.IOException;
 
-import static juego.utilidades.GestionPreguntas.elegirCuestion;
-
 public class MainTest {
 
     public static void main(String[] args) {
-        char correctAnswer = GestionPreguntas.elegirCuestion();
-        System.out.println("La respuesta correcta es la opción: " + correctAnswer);
+
     }
 }
